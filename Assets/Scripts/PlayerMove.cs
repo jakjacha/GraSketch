95fdd3557fab5a16bbd9290d244234ded4be3993
jakubjasinski <jakjacha10@gmail.com>
2019-10-13 12:09:14 +0200

    Dodanie poruszania Prototypowanie AI Enemy

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController _playerMove;
+
+    public float speed;
+    void Start()
+    {
+        _playerMove = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal")*Time.deltaTime*speed, 0f, Input.GetAxisRaw("Vertical")*Time.deltaTime*speed);
+        transform.Translate(input);
+    }
+}
