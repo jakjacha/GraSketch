fef595325b1298d6ad4ebe66da79a6c22525925a
Jakub <jakjacha10@gmail.com>
2019-10-14 23:26:00 +0200

    Dodane życie gracza oraz enemy, początek melee attack i player attack

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth = 100;
+//    private int _healthBarSize;
+//    public int healthBarHeight = 20;
+    void Update()
+    {
+       // _healthBarSize = Screen.width / 2 / (startingHealth / currentHealth);
+        UpdateCurrentHealth(0);
+    }
+//    void OnGUI()
+//    {
+//        GUI.Box(new Rect(Screen.width/4, Screen.height-healthBarHeight, _healthBarSize, healthBarHeight),"test");
+//    }
+
+    public void UpdateCurrentHealth(int change)
+    {
+        currentHealth += change;
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        if (currentHealth > startingHealth)
+            currentHealth = startingHealth;
+    }
+}
--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public GameObject enemy;
+
+    public int damageValue=10;
+
+    public float attackRange=10;
+
+    public float attackAngle=10;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,26 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 range1 = new Vector3(transform.position.x+attackRange,transform.position.y, transform.position.z+attackAngle);
+        Vector3 range2 = new Vector3(transform.position.x+attackRange,transform.position.y, transform.position.z-attackAngle);
+        Debug.DrawLine(transform.position,range1,Color.yellow);
+        Debug.DrawLine(transform.position,range2,Color.yellow);
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        EnemyHealth enemyHealth = (EnemyHealth) enemy.GetComponent("EnemyHealth");
+        enemyHealth.UpdateCurrentHealth(damageValue*(-1));
+    }
+
+    private void FindEnemy()
+    {
+        GameObject[] enemies;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    //    foreach(GameObject)
     }
 }
