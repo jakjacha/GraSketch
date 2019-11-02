7af0d58748bcfb19a1601484bde36ff33350acfd
jakubjasinski <jakjacha10@gmail.com>
2019-11-02 17:01:35 +0100

    mac na pc

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -7,25 +7,27 @@ public class EnemyHealth : MonoBehaviour
 {
     public int startingHealth = 100;
     public int currentHealth = 100;
-//    private int _healthBarSize;
-//    public int healthBarHeight = 20;
+    private int _healthBarSize;
+    //public int _healthBarHeight = 20;
     void Update()
     {
        // _healthBarSize = Screen.width / 2 / (startingHealth / currentHealth);
         UpdateCurrentHealth(0);
+        new Rect(transform.position.x, 2,   10, 10);
     }
-//    void OnGUI()
-//    {
-//        GUI.Box(new Rect(Screen.width/4, Screen.height-healthBarHeight, _healthBarSize, healthBarHeight),"test");
-//    }
 
     public void UpdateCurrentHealth(int change)
     {
         currentHealth += change;
         if (currentHealth < 0)
+        {
             currentHealth = 0;
+            Destroy(gameObject);
+        }
 
         if (currentHealth > startingHealth)
             currentHealth = startingHealth;
+        
+        
     }
 }
--- Assets/Scripts/PlayerFireAttack.cs
+++ Assets/Scripts/PlayerFireAttack.cs
@@ -10,26 +10,52 @@ public class PlayerFireAttack : MonoBehaviour
 
     public float ballSpeed;
     public float fireAttackCooldown;
+
+    private Vector3 playerPos;
     
     private float _ballLifeTime;
-    private float _ballTime;
+    private float _fireAttackCooldownTime;
     void Start()
     {
-        ballSpeed = GetComponent<PlayerMove>().moveSpeed;
+        ballSpeed = GetComponent<PlayerMove>().moveSpeed + 10;
         ball = Resources.Load("Ball") as GameObject;
-        if (ballSpeed < 1) ballSpeed += 10;
+        if (ballSpeed < 1) ballSpeed = 10;
         if (_ballLifeTime < 1) _ballLifeTime = 10;
         if (fireAttackCooldown < 1) fireAttackCooldown = 1;
     }
 
     void Update()
     {
-        Vector3 playerPos = transform.position;
-
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _ballTime)
+        playerPos = transform.position;
+        //attack FWD
+        if (Input.GetKeyDown(KeyCode.I) && Time.time > _fireAttackCooldownTime)
         {
-            Shoot(playerPos);
-            _ballTime = Time.time + fireAttackCooldown;
+            Shoot(playerPos,transform.forward);
+            _fireAttackCooldownTime = Time.time + fireAttackCooldown;
+        }
+        //attack BWD
+        if(Input.GetKeyDown(KeyCode.K) && Time.time > _fireAttackCooldownTime)
+        {
+            Shoot(playerPos, transform.forward, -1);
+            _fireAttackCooldownTime = Time.time + fireAttackCooldown;
+        }
+        //attack Right
+        if (Input.GetKeyDown(KeyCode.L) && Time.time > _fireAttackCooldownTime)
+        {
+            Shoot(playerPos, transform.right);
+            _fireAttackCooldownTime = Time.time + fireAttackCooldown;
+        }
+        //attack Left
+        if (Input.GetKeyDown(KeyCode.J) && Time.time > _fireAttackCooldownTime)
+        {
+            Shoot(playerPos, transform.right, -1);
+            _fireAttackCooldownTime = Time.time + fireAttackCooldown;
+        }
+        
+        //attack ALL
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ShootAll();
         }
         
         //DEBUG
@@ -42,14 +68,23 @@ public class PlayerFireAttack : MonoBehaviour
         Debug.DrawRay(playerPos, new Vector3(0,0,-50), Color.blue );
     }
 
-    private void Shoot(Vector3 pPos)
+    private void Shoot(Vector3 pPos, Vector3 tsf, float dir=1)
     {
         if (!ball) {Debug.Log("Brak obiektu");return;}
         GameObject ballCpy = Instantiate(ball) as GameObject;
-        ballCpy.transform.position = pPos + transform.forward;
+        ballCpy.transform.position = pPos + dir * tsf;
         Rigidbody rb = ballCpy.GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * ballSpeed;
+        rb.velocity = dir * tsf * ballSpeed;
         Destroy(ballCpy, _ballLifeTime);
+    }
+
+    private void ShootAll()
+    {
+        Shoot(playerPos,transform.forward);
+        Shoot(playerPos, transform.forward, -1);
+        Shoot(playerPos, transform.right);
+        Shoot(playerPos, transform.right, -1);
+        
     }
     
     private void OnCollisionEnter(Collision col)
@@ -57,7 +92,7 @@ public class PlayerFireAttack : MonoBehaviour
         if (col.gameObject.CompareTag("Enemy")) Destroy(gameObject);
         
         if(col.gameObject.CompareTag("Wall")) Destroy(gameObject);
-        
+
         Debug.Log(col.gameObject.name);
     }
 }
