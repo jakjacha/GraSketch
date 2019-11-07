c530fced3ca60623b7305a9ebd8ec84332696574
Jakub <jakjacha10@gmail.com>
2019-11-07 14:15:00 +0100

    Dodane PauseMenu

---
+++ Assets/Scripts/MarkerTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerTrigger : MonoBehaviour
+{
+    public GameObject go;
+    // Start is called before the first frame update
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Activating: " + go.name);
+        go.SetActive(true);
+    }
+}
